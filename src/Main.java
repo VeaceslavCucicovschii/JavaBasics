f7235db1b6fe2c7d238198511c3199159b0307d2
Veaceslav Cucicovschii <cucicovschiiveaceslav@gmail.com>
2026-04-14 10:14:21 +0300

    Lambda expressions

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,47 @@
+import LambdaExpressions.MyInterface;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try(FileInputStream file = new FileInputStream("someFile.txt");
-            FileOutputStream fileOut = new FileOutputStream("anotherFile.txt");) {
-            int i;
-            while ((i = file.read()) != -1) {
-                fileOut.write(i);
-            }
+//        try(FileInputStream file = new FileInputStream("someFile.txt");
+//            FileOutputStream fileOut = new FileOutputStream("anotherFile.txt");) {
+//            int i;
+//            while ((i = file.read()) != -1) {
+//                fileOut.write(i);
+//            }
+//
+//            System.out.println("file copied successfully");
+//        }
+//        catch (IOException e) {
+//            System.out.println("Error writing file.");
+//            e.printStackTrace();
+//        }
+//
+//        String text = "\nAppended text!";
+//        try (FileOutputStream output = new FileOutputStream("anotherFile.txt", true)) {
+//            output.write(text.getBytes());
+//
+//            System.out.println("Successfully appended to file.");
+//        }
+//        catch (IOException e) {
+//            System.out.println("Error writing file.");
+//            e.printStackTrace();
+//        }
+        String name = "John";
+        char symbol = '!';
 
-            System.out.println("file copied successfully");
-        }
-        catch (IOException e) {
-            System.out.println("Error writing file.");
-            e.printStackTrace();
-        }
+        MyInterface myInterface = (x, y) -> {
+            System.out.println("Hello World!");
+            System.out.println("It is a nice day! " + x + y);
+        };
 
-        String text = "\nAppended text!";
-        try (FileOutputStream output = new FileOutputStream("anotherFile.txt", true)) {
-            output.write(text.getBytes());
-
-            System.out.println("Successfully appended to file.");
-        }
-        catch (IOException e) {
-            System.out.println("Error writing file.");
-            e.printStackTrace();
-        }
+        MyInterface myInterface1 = (x, y) -> {
+            System.out.println("Hello " + x + y);
+        };
+        
+        myInterface1.message(name, symbol);
     }
 }
